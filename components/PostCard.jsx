bdaf682240ce8583aa0bf8923fb263743b6c5643
VishalGoswami <vishal0711goswami@gmail.com>
2022-07-30 21:27:37 +0530

    new post

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -19,7 +19,7 @@ const PostCard = ({ item }) => {
                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                 </svg>
                 <Moment format="MMM Do, YYYY" className='ml-2'>
-                  <h3 className="tracking-widest text-indigo-500 text-xs ml-2  font-medium title-font">{item.attributes.createdAt}</h3>
+                  <h3 className="tracking-widest text-indigo-500 text-xs ml-2 dark:text-neutral-50 font-medium title-font">{item.attributes.createdAt}</h3>
                 </Moment>
               </div>
 
