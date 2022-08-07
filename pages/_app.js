5280d1ea1f0058d407022f3033be3bcd45b29a02
VishalGoswami <vishal0711goswami@gmail.com>
2022-08-07 16:30:22 +0530

    ads

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,44 @@ import '../styles/globals.css'
 import Head from "next/head"
 import NextNProgress from "nextjs-progressbar";
 import Footer from '../components/Footer';
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps}) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return( 
   <>
+   <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
   <Head>
     <title>Manuscripts</title>
   </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* eslint-disable-next-line */}
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5303359554216037"
+          crossorigin="anonymous"></script>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous" />
           <link rel="shortcut icon" href="/images/logo.png" />
         </Head>
